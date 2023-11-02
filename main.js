b334476320d2646a8b561d3b9daf7c240fb3366a
Mikaeljafet <143093462+Mikaeljafet@users.noreply.github.com>
2023-11-02 19:00:04 +0000

    r

--- main.js
+++ main.js
@@ -72,9 +72,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
 /** Bæta vöru í körfu */
 function addProductToCart(product, quantity) {
   // Hér þarf að finna `<tbody>` í töflu og setja `cartLine` inn í það
-  const cartTableBody = document.querySelector('.cart-table tbody');
+  const cart = document.querySelector('.cart-table tbody');
 
-  if (!cartTableBody) {
+  if (!cart) {
     console.warn('fann ekki .cart-content tbody');
     return;
   }
